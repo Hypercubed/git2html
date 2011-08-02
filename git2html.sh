Hypercubed/git2html

commit ed7be6464b43c442be9509efd2d9989a0f7bb0d8

Sort the files in the file hierarchy so that files preceed sub-directories.

git2html.sh
@@ -381,8 +381,14 @@ do
       echo "<p>Files:" \
         "<ul>"
 
-      # The list of files as a hierarchy.
-      gawk 'function spaces(l) {
+      # The list of files as a hierarchy.  Sort them so that within a
+      # directory, files preceed sub-directories
+      sed 's/\([^ \t]\+[ \t]\)\{3\}//;
+                 s#^#/#; s#/\([^/]*/\)#/1\1#; s#/\([^/]*\)$#/0\1#;' \
+          < "$FILES" \
+	  | sort | sed 's#/[01]#/#g; s#^/##' \
+	  | gawk '
+           function spaces(l) {
              for (space = 1; space <= l; space ++) { printf ("  "); }
            }
            function max(a, b) { if (a > b) { return a; } return b; }
@@ -402,7 +408,7 @@ do
              delete current_components[1];
            }
            {
-             file=$4;
+             file=$0;
              split(file, components, "/")
              # Remove the file.  Keep the directories.
              file=components[length(components)]
@@ -452,7 +458,7 @@ do
                printf ("</ul> <!-- %s -->\n", current_components[j]);
                delete current_components[j];
              }
-           }' < "$FILES"
+           }'
 
       echo "</ul>"
       html_footer

commit 374de1f046a14c25d612d25389355010eddcc9b4

Include the filename when linking to a a file.
Fix regression introduced in ed7be6464b43c442be9509efd2d9989a0f7bb0d8.

git2html.sh
@@ -453,7 +453,7 @@ do
   
              spaces(length(current_components))
              printf ("<li><a name=\"files:%s\" href=\"%s.raw.html\">%s</a>\n",
-                     $4, $4, file);
+                     $0, $0, file);
            }
 
            END {